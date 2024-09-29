cab681170c14ea48392faa94fd057d922382fc06
EvgenyChernov <evgenycher89@gmail.com>
2024-09-29 14:41:01 +1200

    Сделал двустороннее связывание input

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const { createApp } = Vue
+const {createApp} = Vue
 
 createApp({
     data() {
         return {
-            count : 3,
-            title: 'Счетчик'
+            title: 'Список заметок',
+            placeholderString: 'Введите название заметки',
+            inputValue: ''
+
+        }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            console.log('inputChangeHandler', event.target.value)
+            this.inputValue = event.target.value
         }
     }
 }).mount('#app')
