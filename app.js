a978d0dee6684c78bfecb0ec277f7051a277102b
EvgenyChernov <evgenycher89@gmail.com>
2024-10-01 11:09:04 +1200

    пример построения и парсинга витруального ДОМ

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
-Vue.createApp({
-    data: () => ({
-        myHtml: '<h1>vue 3 app </h1>',
-        title: 'Vue Youtube',
-        user: {
-            name: 'evgen',
-            email: 'evgen@gmail.com',
-            password: 'evgen@gmail.com',
-            age: '23'
-        },
-        items: [1, 2, 3, 4, 5, 6],
-    }),
-    methods: {
-        addItem(event) {
-            this.items.unshift(this.$refs.myInput.value)
-            this.$refs.myInput.value = ''
-            console.log(event.key)
-        },
-        remove(index){
-            this.items.splice(index, 1)
-        },
-        log(event){
-            console.log(event)
+const h = Vue.h
+
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'это title'
         }
     },
-    computed: {
-        evenItems() {
-            return this.items.filter(i => i % 2 === 0);
-        },
+    // template: `
+    // <div>
+    //        <h1>Вывод из темплейта</h1>
+    //        <h1>{{ title }}</h1>
+    //        <button @click="title = 'новый title'" class="btn">изменить</button>
+    // </div>
+    // `,
+    methods: {
+        changeTitle() {
+            this.title = 'новый тайтл'
+        }
+    },
+    render() {
+        return h('div', {
+                class: 'card center'
+            },
+            h('ul', null, [
+                h('li', null,  this.title ),
+                h('li', null, 'это Li2')
+            ]),
+            h('button', {
+                class: 'btn error',
+                onclick: this.changeTitle
+            }, 'Изменить')
+        )
     }
-}).mount('#app')
+})
+
+
+app.mount('#app')
