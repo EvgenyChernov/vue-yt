cd9643a2efb87a25ad8e9d71823e76ce5448354f
EvgenyChernov <evgenycher89@gmail.com>
2024-10-02 11:55:58 +1200

    создание первого компонента проброс данных

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
-<template>
-  <h1>Hello world</h1>
-</template>
-
 <script>
+import AppNews from '@/AppNews.vue'
 
 export default {
-  name: 'App',
+  data () {
+    return {
+      now: new Date().toLocaleDateString(),
+      news: [
+        'Какая то новость',
+        'Какая то новость2'
+      ]
+    }
+  },
   components: {
-
+    AppNews
   }
 }
 </script>
+
+<template>
+  <div class="container pt-1">
+    <div class="card ">
+      <h2>Актуальные новости на {{ now }}</h2>
+      <app-news></app-news>
+      <app-news
+          v-for="item in news"
+          :key="item"
+          :title="item"
+      ></app-news>
+    </div>
+  </div>
+
+</template>
 
 <style lang="scss">
 #app {
---
+++ src/AppNews.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: [
+    'title'
+  ],
+  data () {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h3>{{ item }} + {{ title }}</h3>
+    <button @click="isOpen = !isOpen" class="btn btn-primary btn-sm">Открыть</button>
+    <p v-if="isOpen">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi placeat quibusdam sint
+      temporibus
+      totam?</p>
+  </div>
+</template>
