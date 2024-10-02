ec61a7fcaff872aacb643c3b982242b2e118d349
EvgenyChernov <evgenycher89@gmail.com>
2024-10-02 22:44:06 +1200

    работа со слотом, доступ к данным проброс данных

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container pt-1">
     <h1>Slots</h1>
+    <app-list>
+      <template #default="{inx, iter}">
+        <span  style="color: #6ddb09;"> <strong>{{inx}}</strong> item: {{iter}}</span>
+      </template>
+    </app-list>
     <app-block>
       <template #footer>
         <p>Это самый важный текст для этого приложения</p>
@@ -14,8 +19,16 @@
 
 <script>
 import AppBlock from '@/AppBlock.vue'
+import AppList from '@/AppList.vue'
+import colors from 'tailwindcss/colors'
+
 export default {
-  components: { AppBlock }
+  methods: {
+    colors () {
+      return colors
+    }
+  },
+  components: { AppBlock, AppList }
 }
 </script>
 
---
+++ src/AppList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, i) in 5" :key="item">
+        <slot :iter="item" :inx="i"></slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
