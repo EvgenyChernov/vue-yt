d0910a42f8ee30960e969fd25cad2f28bf7b1f9b
EvgenyChernov <evgenycher89@gmail.com>
2024-10-04 17:43:10 +1200

    создал простую форму создания пользователя

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="container mx-auto">
     <div class="bg-white rounded-3xl w-auto p-7">
+      <form class="form-control space-y-5" @submit.prevent="createPerson">
+        <label class="form-control w-full max-w-xs">
+          <div class="label">
+            <span class="label-text">What is your name?</span>
+          </div>
+          <input
+              v-model="name"
+              type="text"
+              placeholder="Type here"
+              class="input input-bordered w-full max-w-xs"
+          />
+        </label>
+        <div class="">
+          <button class="btn" :disabled="name.length === 0">Создать человека</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -8,7 +24,15 @@
 <script>
 
 export default {
-
+  data () {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    createPerson () {
+    }
+  }
 }
 </script>
 
