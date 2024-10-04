970b98a23441e6b55900d4efb885ee2606b432f3
EvgenyChernov <evgenycher89@gmail.com>
2024-10-04 18:54:40 +1200

    создание запроса к базе данных на получение пользователей axios fetch парсинг полученных данных и вывод

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto">
+  <div class="container mx-auto space-y-5">
     <div class="bg-white rounded-3xl w-auto p-7">
       <form class="form-control space-y-5" @submit.prevent="createPerson">
         <label class="form-control w-full max-w-xs">
@@ -18,15 +18,24 @@
         </div>
       </form>
     </div>
+
+    <app-people-list
+        @load="loadPeople"
+        :people="people"
+    ></app-people-list>
+
   </div>
 </template>
 
 <script>
+import AppPeopleList from '@/AppPeopleList.vue'
+import axios from 'axios'
 
 export default {
   data () {
     return {
-      name: ''
+      name: '',
+      people: []
     }
   },
   methods: {
@@ -45,8 +54,18 @@ export default {
       const fireBaseData = await response.json()
       console.log(fireBaseData)
       this.name = ''
+    },
+    async loadPeople () {
+      const { data } = await axios.get('https://vue-yt-2f717-default-rtdb.europe-west1.firebasedatabase.app/people.json')
+      this.people = Object.keys(data).map(key => {
+        return {
+          id: key,
+          ...data[key]
+        }
+      })
     }
-  }
+  },
+  components: { AppPeopleList }
 }
 </script>
 
---
+++ src/AppPeopleList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="people.length !== 0" class="bg-white rounded-3xl w-auto p-7">
+    <div
+        v-for="person in people"
+        :key="person.id"
+    >{{ person.firstName }}</div>
+  </div>
+  <div v-else class="bg-white rounded-3xl w-auto p-7 flex justify-center">
+    <button class="btn btn-success" @click="$emit('load')">Загрузить пользователей</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['load'],
+  props: ['people']
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
