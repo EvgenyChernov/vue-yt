178c80be5913db1395f352472f28c626e4053e64
EvgenyChernov <evgenycher89@gmail.com>
2024-10-07 18:19:17 +1200

    Добавлен плагин и использование

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container mx-auto space-y-3 pt-3">
     <div class="bg-white rounded-xl p-5">
+      <h2>Как работают плагины</h2>
+      <button @click="$alert('Наш алерт2')" class="btn">Алерт</button>
     </div>
   </div>
 </template>
@@ -8,6 +10,9 @@
 <script>
 
 export default {
+  mounted () {
+    this.$alert('Наш алерт')
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import translatePlugin from '@/translatePlugin'
 
 const app = createApp(App)
+app.use(translatePlugin)
 app.mount('#app')
