acf3029deccb15295e99e85dc5d94bdcd27bf968
EvgenyChernov <evgenycher89@gmail.com>
2024-10-07 16:46:19 +1200

    Очистка проекта

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,13 @@
 <template>
   <div class="container mx-auto space-y-3 pt-3">
     <div class="bg-white rounded-xl p-5">
-      <h2 v-color:[type].hover="myColor">Директивы</h2>
-      <div v-color:[type].blink="myColor" class="flex flex-col">
-        <label for="inp">Активный по умолчанию</label>
-        <input v-focus  type="text" id="inp">
-      </div>
-      <button @click="myColor = 'green'" class="btn">сделать </button>
-      <button @click="type = type === 'color'? 'backgroundColor' : 'color'" class="btn">поменять цвет </button> {{type}}
     </div>
   </div>
 </template>
 
 <script>
 
-import focusDirective from '@/focusDirective'
-import colorDirective from '@/colorDirective'
-
 export default {
-  directives: {
-    focus: focusDirective,
-    color: colorDirective
-  },
-  data () {
-    return {
-      myColor: 'red',
-      type: 'color'
-    }
-  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
-// import alertMixin from '@/alertMixin'
 
 const app = createApp(App)
-
-// app.directive('focus', {
-//   mounted (el) {
-//     el.focus()
-//   }
-// })
-// app.mixin(alertMixin)
 app.mount('#app')
