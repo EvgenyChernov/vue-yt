748230bc7743d6f14c8321d61d6fed29a78ccef0
EvgenyChernov <evgenycher89@gmail.com>
2024-10-07 19:57:35 +1200

    плагин по изменению языка но используется $forceUpdate

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="container mx-auto space-y-3 pt-3">
     <div class="bg-white rounded-xl p-5">
-      <h2>Как работают плагины</h2>
+      <h2>{{ $i18n('app.title')}}</h2>
       <button @click="$alert('Наш алерт2')" class="btn">Алерт</button>
     </div>
+    <button @click="changeLang" class="btn">{{$i18n('app.changeBtn')}}</button>
   </div>
 </template>
 
 <script>
 
 export default {
+  inject: ['changeI18N'],
   mounted () {
     this.$alert('Наш алерт')
+  },
+  methods: {
+    changeLang () {
+      this.changeI18N('en')
+      this.$forceUpdate()
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,20 @@ import './assets/tailwind.css'
 import translatePlugin from '@/translatePlugin'
 
 const app = createApp(App)
-app.use(translatePlugin)
+
+const ru = {
+  app: {
+    title: 'как работают плагины во vue',
+    changeBtn: 'изменить язык'
+  }
+}
+
+const en = {
+  app: {
+    title: 'How plugins work in vue',
+    changeBtn: 'change language'
+  }
+}
+
+app.use(translatePlugin, { ru, en })
 app.mount('#app')
