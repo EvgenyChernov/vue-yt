79f930c18a4af3a83dff8eb08dd2d46be1543b24
EvgenyChernov <evgenycher89@gmail.com>
2024-10-08 21:25:46 +1200

    очистка проекта

--- src/main.js
+++ src/main.js
@@ -1,25 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import './assets/tailwind.css'
-import translatePlugin from '@/translatePlugin'
 
 const app = createApp(App)
 
-const ru = {
-  app: {
-    title: 'как работают плагины во vue',
-    changeBtn: 'изменить язык'
-  }
-}
-
-const en = {
-  app: {
-    title: 'How plugins work in vue',
-    changeBtn: 'change language'
-  }
-}
-
-app.use(translatePlugin, { ru, en })
-app.use(router)
 app.mount('#app')
