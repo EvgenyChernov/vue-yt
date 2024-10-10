1711079309aead713ac21edcc3f67238d65d8cf5
EvgenyChernov <evgenycher89@gmail.com>
2024-10-10 21:14:34 +1200

    Очистил компоненты кроме стора

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from "@/views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/users',
-    name: 'users',
-    component: () => import('../views/UsersView.vue')
   }
 ]
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,10 @@
 <template>
   <div>
-    Домашняя страница
-    {{ text }}
-    <button @click="navigate" class="btn">перейти</button>
+
   </div>
 </template>
 
 <script setup lang="ts">
-import {useRouter} from "vue-router";
-
-const router = useRouter()
-const text = 'Some text'
-const navigate = () => router.push({name: 'users'})
-
 
 </script>
 
